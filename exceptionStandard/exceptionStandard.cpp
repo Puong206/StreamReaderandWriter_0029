1142ace9ec4da11d2122fa1c5a0aedc2f6c390bc
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2024-06-04 18:35:26 +0700

    exceptionStandard : fix folder name

---
+++ exceptionStandard/exceptionStandard.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <exception>
+// untuk objek exception yang akan digunakan
+#include <array>
+// untuk objek array yang akan digunakan
+using namespace std;
+
+int main() {
+	cout << "Awal Program" << endl; //Penanda 1:...
+	try
+	{
+		array<int, 3> data = { 6, 7, 8 };
+		// pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		// memanggil array elemen ke 5
+	}
+	catch (exception& e)
+	{
+		//penangkap menggunakan objek exception
+		cout << e.what() << endl;
+		/*akan dieksekusi karena array data hanya memiliki 3 elemen*/
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	/* Penanda 2 : Bahwa Program berjalan tanpa berhenti meskipun terjadi kesalahan */
+	return 0;
+}
